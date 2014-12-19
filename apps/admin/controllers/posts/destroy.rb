dee829ef6466051ff5654f9251afd1ed2a27b6d6
Trung Lê <trung.le@ruby-journal.com>
2014-12-19 11:22:00 +1100

    Now you can destroy post

---
+++ apps/admin/controllers/posts/destroy.rb
@@ -0,0 +1,12 @@
+module Admin::Controllers::Posts
+  class Destroy
+    include Admin::Action
+
+    def call(params)
+      @post = PostRepository.find(params[:id])
+      PostRepository.delete(@post)
+
+      redirect_to '/admin/posts'
+    end
+  end
+end

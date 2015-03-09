fb027f665780537dfc615b7aed7058241aa505f5
Vinh Nguyen Le <vinh.nguyenlexuan@asnet.com.vn>
2015-03-09 20:34:13 +0700

    Add API get post detail

---
+++ spec/api/v1/requests/posts/show_spec.rb
@@ -0,0 +1,31 @@
+require 'api_helper'
+
+describe 'GET /api/v1/posts/1' do
+
+  before do
+    UserRepository.clear
+    PostRepository.clear
+
+    _user = User.new(first_name: 'Vinh', last_name: 'Nguyen', email: 'vinh.nguyenlexuan@gmail.com')
+    UserRepository.persist(_user)
+    @user = UserRepository.last
+
+    _post = Post.new(user_id: @user.id, content: 'lorem ipsum', title: 'lorem ipsum')
+    PostRepository.persist(_post)
+    @post = PostRepository.last
+  end
+
+  it 'returns all posts' do
+    get "/api/v1/posts/#{@post.id}"
+
+    response.status.must_equal 200
+    body.must_equal(
+      {
+        'id' => @post.id,
+        'title'=>'lorem ipsum',
+        'content'=>'lorem ipsum',
+        'user_id'=> @user.id
+      }
+    )
+  end
+end
